d4136805f3bb4fcfb6ab8e9d9d7a4cbf2c69a21e
totegamma <github@gammalab.net>
2023-12-03 00:44:23 +0900

    add tag/scope

---
+++ x/core/scope_test.go
@@ -0,0 +1,20 @@
+package core
+
+import (
+    "testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestScopes(t *testing.T) {
+    scope1 := ParseScopes("stream:write:ci8qvhep9dcpltmfq3fg@hub.concurrent.world")
+    if assert.Contains(t, scope1.Body, "stream") {
+        assert.Equal(t, scope1.Body["stream"].Action[0], "write")
+        assert.Equal(t, scope1.Body["stream"].Resources[0], "ci8qvhep9dcpltmfq3fg@hub.concurrent.world")
+        assert.Equal(t, scope1.CanPerform("stream:write:ci8qvhep9dcpltmfq3fg@hub.concurrent.world"), true)
+        assert.Equal(t, scope1.CanPerform("stream:read:ci8qvhep9dcpltmfq3fg@hub.concurrent.world"), false)
+    }
+
+    scope2 := ParseScopes("*:*:*")
+    assert.Equal(t, scope2.CanPerform("stream:write:ci8qvhep9dcpltmfq3fg@hub.concurrent.world"), true)
+}
+

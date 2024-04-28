b458574d3236ceef7b2e3f3357e1570b9278d65b
totegamma <github@gammalab.net>
2024-04-28 19:34:07 +0900

    add user log

--- x/agent/main.go
+++ x/agent/main.go
@@ -61,14 +61,19 @@ func (a *agent) FlushLog(ctx context.Context) {
 
 	slog.Info("flush log Start")
 
-	err := os.MkdirAll(a.config.Server.RepositoryPath, 0755)
+	allPath := filepath.Join(a.config.Server.RepositoryPath, "/all")
+
+	err := os.MkdirAll(allPath, 0755)
 	if err != nil {
 		slog.Error("failed to create repository directory:", err)
 		panic(err)
 	}
 
-	logpath := filepath.Join(a.config.Server.RepositoryPath, "all.log")
-	storage, err := os.OpenFile(logpath, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	timestamp := time.Now().Format("2006-01-02")
+	filename := fmt.Sprintf("%s.log", timestamp)
+
+	alllogpath := filepath.Join(allPath, filename)
+	storage, err := os.OpenFile(alllogpath, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		slog.Error("failed to open repository log file:", err)
 		panic(err)
@@ -138,6 +143,36 @@ func (a *agent) FlushLog(ctx context.Context) {
 	}
 
 	storage.WriteString(log)
+
+	// flush to each user log
+
+	userlogPath := filepath.Join(a.config.Server.RepositoryPath, "/user")
+	err = os.MkdirAll(userlogPath, 0755)
+	if err != nil {
+		slog.Error("failed to create repository directory:", err)
+		panic(err)
+	}
+
+	bucket := make(map[string]string)
+	for _, entry := range entries {
+		if _, ok := bucket[entry.Owner]; !ok {
+			bucket[entry.Owner] = ""
+		}
+		bucket[entry.Owner] += fmt.Sprintf("%s %s %s\n", entry.ID, entry.Owner, entry.Content)
+	}
+
+	for owner, log := range bucket {
+		filename := fmt.Sprintf("%s.log", owner)
+		logpath := filepath.Join(userlogPath, filename)
+		userstore, err := os.OpenFile(logpath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			slog.Error("failed to open repository log file:", err)
+			continue
+		}
+		defer userstore.Close()
+
+		userstore.WriteString(log)
+	}
 
 	slog.Info(fmt.Sprintf("%d entries flushed", len(entries)))
 }

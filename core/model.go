4d4cdb832706a3bb4877d3785cde405677907edb
totegamma <github@gammalab.net>
2024-05-03 02:59:01 +0900

    cleanup

--- core/model.go
+++ core/model.go
@@ -15,3 +15,28 @@ type Chunk struct {
 	Key   string         `json:"key"`
 	Items []TimelineItem `json:"items"`
 }
+
+type RequestContext struct {
+	Requester Entity
+	Document  any
+	Self      any
+	Params    map[string]any
+}
+
+type Policy struct {
+	Name       string      `json:"name"`
+	Version    string      `json:"version"`
+	Statements []Statement `json:"statements"`
+}
+
+type Statement struct {
+	Action    []string `json:"action"`
+	Effect    string   `json:"effect"`
+	Condition Expr     `json:"condition"`
+}
+
+type Expr struct {
+	Operator string `json:"op"`
+	Args     []Expr `json:"args"`
+	Constant any    `json:"constant"`
+}

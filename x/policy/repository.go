766844c3773d4846cc7f3a0802f642b181e6067c
totegamma <github@gammalab.net>
2025-02-08 19:26:20 +0900

    update policy accuire logic

--- x/policy/repository.go
+++ x/policy/repository.go
@@ -34,8 +34,12 @@ func (r *repository) Get(ctx context.Context, url string) (core.Policy, error) {
 	ctx, span := tracer.Start(ctx, "Policy.Repository.Get")
 	defer span.End()
 
+	var cache *core.Policy = nil
+
 	// check cache
 	key := fmt.Sprintf("policy:%s", url)
+	lockKey := fmt.Sprintf("lock:%s", key)
+
 	val, err := r.rdb.Get(ctx, key).Result()
 	if err == nil {
 		var policy core.Policy
@@ -44,67 +48,95 @@ func (r *repository) Get(ctx context.Context, url string) (core.Policy, error) {
 			span.SetStatus(codes.Error, err.Error())
 			return core.Policy{}, err
 		}
-		return policy, nil
+		cache = &policy
 	}
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		return core.Policy{}, err
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		return core.Policy{}, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		err = fmt.Errorf("unexpected status code: %d", resp.StatusCode)
-		span.SetStatus(codes.Error, err.Error())
-		return core.Policy{}, err
-	}
-
-	jsonStr, err := io.ReadAll(resp.Body)
-	if err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		return core.Policy{}, err
-	}
-
-	// cache policy
-	var policyDoc core.PolicyDocument
-	err = json.Unmarshal(jsonStr, &policyDoc)
-	if err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		return core.Policy{}, err
-	}
-
-	var policy core.Policy
-	policy20240701, ok := policyDoc.Versions["2024-07-01"]
-	if ok {
-		span.AddEvent("use version 2024-07-01")
-		policy = policy20240701
-	} else {
-		span.AddEvent("fallback to latest version")
-		err = json.Unmarshal(jsonStr, &policy)
+	fetch := func() (core.Policy, error) {
+		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
 			span.SetStatus(codes.Error, err.Error())
 			return core.Policy{}, err
 		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			span.SetStatus(codes.Error, err.Error())
+			return core.Policy{}, err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			err = fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+			span.SetStatus(codes.Error, err.Error())
+			return core.Policy{}, err
+		}
+
+		jsonStr, err := io.ReadAll(resp.Body)
+		if err != nil {
+			span.SetStatus(codes.Error, err.Error())
+			return core.Policy{}, err
+		}
+
+		// cache policy
+		var policyDoc core.PolicyDocument
+		err = json.Unmarshal(jsonStr, &policyDoc)
+		if err != nil {
+			span.SetStatus(codes.Error, err.Error())
+			return core.Policy{}, err
+		}
+
+		var policy core.Policy
+		policy20240701, ok := policyDoc.Versions["2024-07-01"]
+		if ok {
+			span.AddEvent("use version 2024-07-01")
+			policy = policy20240701
+		} else {
+			span.AddEvent("fallback to latest version")
+			err = json.Unmarshal(jsonStr, &policy)
+			if err != nil {
+				span.SetStatus(codes.Error, err.Error())
+				return core.Policy{}, err
+			}
+		}
+
+		jsonStr, err = json.Marshal(policy)
+		if err != nil {
+			span.SetStatus(codes.Error, err.Error())
+			return core.Policy{}, err
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		err = r.rdb.Set(ctx, key, jsonStr, 0).Err()
+		if err != nil {
+			span.SetStatus(codes.Error, err.Error())
+			return core.Policy{}, err
+		}
+
+		err = r.rdb.Set(ctx, lockKey, "1", 5*time.Minute).Err()
+		if err != nil {
+			span.SetStatus(codes.Error, err.Error())
+			return core.Policy{}, err
+		}
+
+		return policy, nil
 	}
 
-	jsonStr, err = json.Marshal(policy)
+	if cache == nil {
+		return fetch()
+	}
+
+	// check lock
+	exists, err := r.rdb.Exists(ctx, lockKey).Result()
 	if err != nil {
 		span.SetStatus(codes.Error, err.Error())
-		return core.Policy{}, err
+		return *cache, nil
 	}
 
-	err = r.rdb.Set(ctx, key, jsonStr, 10*time.Minute).Err() // 10 minutes
-	if err != nil {
-		span.SetStatus(codes.Error, err.Error())
-		return core.Policy{}, err
+	if exists == 0 {
+		go fetch()
 	}
 
-	return policy, nil
+	return *cache, nil
 }

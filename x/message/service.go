1e3fcb38b0b591f30fe422f6d416fa1c36e12a2a
totegamma <github@gammalab.net>
2025-09-21 02:40:43 +0900

    fix realtime message cdate

--- x/message/service.go
+++ x/message/service.go
@@ -7,6 +7,7 @@ import (
 	"log/slog"
 	"slices"
 	"strings"
+	"time"
 
 	"github.com/pkg/errors"
 	"go.opentelemetry.io/otel/codes"
@@ -410,6 +411,7 @@ func (s *service) Create(ctx context.Context, mode core.CommitMode, document str
 			PolicyDefaults: policydefaults,
 			Document:       document,
 			Signature:      signature,
+			CDate:          time.Now(),
 			Timelines:      doc.Timelines,
 		}
 	}

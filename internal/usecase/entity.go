76c2e01dfba52f34cc924f1db881b901604dd004
totegamma <github@gammalab.net>
2026-05-07 23:02:00 +0900

    small fix

--- internal/usecase/entity.go
+++ internal/usecase/entity.go
@@ -47,7 +47,7 @@ func (uc *EntityUsecase) Register(ctx context.Context, req concrnt.RegisterReque
 	var inviter *string
 	switch uc.config.Registration {
 	case "invite":
-		if req.InviteToken != nil {
+		if req.InviteToken == nil {
 			err := domain.PermissionError{Reason: "invite token is not supported in this implementation"}
 			span.RecordError(err)
 			return err
